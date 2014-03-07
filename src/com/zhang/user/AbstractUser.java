be18815b5abde1be38d036451fb6c8362bc0e8e6
niannianli <niannian_li@qq.com>
2014-03-07 13:58:18 -0500

    1

---
+++ src/com/zhang/user/AbstractUser.java
@@ -0,0 +1,30 @@
+package com.zhang.user;
+
+/**
+ * AbstractUser entity provides the base persistence definition of the User
+ * entity. @author MyEclipse Persistence Tools
+ */
+
+public abstract class AbstractUser implements java.io.Serializable {
+
+	// Fields
+
+	private Long userId;
+
+	// Constructors
+
+	/** default constructor */
+	public AbstractUser() {
+	}
+
+	// Property accessors
+
+	public Long getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+}
---
+++ src/com/zhang/user/User.java
@@ -0,0 +1,14 @@
+package com.zhang.user;
+
+/**
+ * User entity. @author MyEclipse Persistence Tools
+ */
+public class User extends AbstractUser implements java.io.Serializable {
+
+	// Constructors
+
+	/** default constructor */
+	public User() {
+	}
+
+}
